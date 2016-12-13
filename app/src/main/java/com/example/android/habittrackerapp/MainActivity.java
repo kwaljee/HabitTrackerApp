9cf4a3fc35bd1ca2240dfef5671a028ad01effad
Karim <kwaljee@gmail.com>
2016-12-13 12:07:50 -0500

    Updated code with Cursor object

--- app/src/main/java/com/example/android/habittrackerapp/MainActivity.java
+++ app/src/main/java/com/example/android/habittrackerapp/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         mDbHelper = new HabitTrackerDbHelper(this);
         displayDatabaseInfo();
     }
@@ -52,38 +51,10 @@ public class MainActivity extends AppCompatActivity {
         TextView displayView = (TextView) findViewById(R.id.textView);
 
         try {
-            // Create a header in the Text View that looks like this:
-            //
-            // The pets table contains <number of rows in Cursor> habits.
-            // _id - name - progress
-            //
-            // In the while loop below, iterate through the rows of the cursor and display
-            // the information from each column in this order.
+
             insertHabit();
             displayView.setText("The habits table contains " + cursor.getCount() + " habits.\n\n");
-            displayView.append(HabitEntry._ID + " - " +
-                    HabitEntry.COLUMN_HABIT_NAME + " - " +
-                    HabitEntry.COLUMN_PROGRESS + "\n");
 
-            // Figure out the index of each column
-            int idColumnIndex = cursor.getColumnIndex(HabitEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(HabitEntry.COLUMN_HABIT_NAME);
-            int progressColumnIndex = cursor.getColumnIndex(HabitEntry.COLUMN_PROGRESS);
-
-
-            // Iterate through all the returned rows in the cursor
-            while (cursor.moveToNext()) {
-                // Use that index to extract the String or Int value of the word
-                // at the current row the cursor is on.
-                int currentID = cursor.getInt(idColumnIndex);
-                String currentName = cursor.getString(nameColumnIndex);
-                String currentProgress = cursor.getString(progressColumnIndex);
-
-                // Display the values from each column of the current row in the cursor in the TextView
-                displayView.append(("\n" + currentID + " - " +
-                        currentName + " - " +
-                        currentProgress));
-            }
         } finally {
             // Always close the cursor when you're done reading from it. This releases all its
             // resources and makes it invalid.
